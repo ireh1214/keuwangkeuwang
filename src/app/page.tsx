8d6d180d0880885b7ee35b2414fdc3faf8591b2c
irpark@enliple.com <irpark@enliple.com>
2024-02-08 12:03:30 +0900

    초기세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
+import CommonInput from "@/components/CommonInput";
+import Title from "@/components/Title";
 import "../scss/common.scss";
 
 
 export default function Home() {
   return (
     <main>
-      hello, world!
+      컴포넌트 작성
+      <CommonInput />
+      <Title title="안녕하세요! Hello World!" />
     </main>
   );
 }
---
+++ src/components/Title.tsx
@@ -0,0 +1,8 @@
+
+
+
+export default function Title(props) {
+    return (
+        <h3 className="title">{props.title}</h3>
+    );
+}
